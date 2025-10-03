d3fea2ae81fc849f233984a0cae2e3c39de43803
Valery Angelique <71804886+vangeliq@users.noreply.github.com>
2025-10-03 00:26:35 +0000

    sanity check - comment out build_gif stuff

--- src/cmake/build_GIF.cmake
+++ src/cmake/build_GIF.cmake
@@ -15,26 +15,29 @@ super_set (GIF_BUILD_GIT_TAG "${GIF_BUILD_VERSION}")
 set_cache (GIF_BUILD_SHARED_LIBS ${LOCAL_BUILD_SHARED_LIBS_DEFAULT}
            DOC "Should execute a local GIFLIB build; if necessary, build shared libraries" ADVANCED)
 
-string (MAKE_C_IDENTIFIER ${GIF_BUILD_VERSION} GIF_VERSION_IDENT)
+# string (MAKE_C_IDENTIFIER ${GIF_BUILD_VERSION} GIF_VERSION_IDENT)
 
-build_dependency_with_cmake(GIF
-    VERSION         ${GIF_BUILD_VERSION}
-    GIT_REPOSITORY  ${GIF_BUILD_GIT_REPOSITORY}
-    GIT_TAG         ${GIF_BUILD_GIT_TAG}
-    # pass the path to our custom CMakeLists.txt template
-    CMAKELISTS_TEMPLATE "${CMAKE_CURRENT_LIST_DIR}/build_GIF_CMakeLists.txt"
-    CMAKE_ARGS
-        -D BUILD_SHARED_LIBS=${GIF_BUILD_SHARED_LIBS}
-)
+# build_dependency_with_cmake(GIF
+#     VERSION         ${GIF_BUILD_VERSION}
+#     GIT_REPOSITORY  ${GIF_BUILD_GIT_REPOSITORY}
+#     GIT_TAG         ${GIF_BUILD_GIT_TAG}
+#     # pass the path to our custom CMakeLists.txt template
+#     CMAKELISTS_TEMPLATE "${CMAKE_CURRENT_LIST_DIR}/build_GIF_CMakeLists.txt"
+#     CMAKE_ARGS
+#         -D BUILD_SHARED_LIBS=${GIF_BUILD_SHARED_LIBS}
+# )
 
-# Set some things up that we'll need for a subsequent find_package to work
-set (GIF_ROOT ${GIF_LOCAL_INSTALL_DIR})
+# # Set some things up that we'll need for a subsequent find_package to work
+# set (GIF_ROOT ${GIF_LOCAL_INSTALL_DIR})
 
-# Signal to caller that we need to find again at the installed location
-set (GIF_REFIND TRUE)
-set (GIF_REFIND_VERSION ${GIF_BUILD_VERSION})
-set (GIF_REFIND_ARGS CONFIG)
+# find_package(GIF REQUIRED CONFIG PATHS "D:/a/OpenImageIO/OpenImageIO/build/deps/dist/lib/cmake")
+# message(STATUS "GIF found at ${GIF_DIR}")
 
-if (GIF_BUILD_SHARED_LIBS)
-    install_local_dependency_libs (GIF GIF)
-endif ()
+# # Signal to caller that we need to find again at the installed location
+# set (GIF_REFIND TRUE)
+# set (GIF_REFIND_VERSION ${GIF_BUILD_VERSION})
+# set (GIF_REFIND_ARGS CONFIG)
+
+# if (GIF_BUILD_SHARED_LIBS)
+#     install_local_dependency_libs (GIF GIF)
+# endif ()
